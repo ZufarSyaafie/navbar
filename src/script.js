5cfb07b23d8d00d6dff895bfe859a3cb933e34ca
ZufarSyaafie <zufar.syaafie@gmail.com>
2024-01-17 16:07:16 +0700

    Menambahkan menu setelah hamburger ditekan

--- src/script.js
+++ src/script.js
@@ -1,5 +1,9 @@
 const hamburger_menu = document.querySelector('.hamburger-menu');
+const mobile_menu = document.querySelector('.menu-mobile');
+const navbar = document.querySelector('.navbar');
 
 hamburger_menu.addEventListener('click', function(){
     hamburger_menu.classList.toggle('is-active');
+    mobile_menu.classList.toggle('is-active');
+    navbar.classList.toggle('is-active');
 });
